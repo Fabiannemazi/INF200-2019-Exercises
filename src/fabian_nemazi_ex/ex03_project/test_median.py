aa5137259227632c62160a91c6cd291a0f00a67a
Fabian Nemazi <fabinema@nmbu.no>
2019-10-04 14:13:26 +0200

    updated the files

--- src/fabian_nemazi_ex/ex03_project/test_median.py
+++ src/fabian_nemazi_ex/ex03_project/test_median.py
@@ -1,72 +1,106 @@
+import pytest
+import random
+
+
 __author__ = 'Fabian Nemazi'
 __email__ = 'fabinema@nmbu.no'
 
 
+#def median(data):
+    #sdata = sorted(data)
+    #n = len(sdata)
+    #if n == 0:
+        #raise ValueError
+    #return (sdata[n // 2] if n % 2 == 1
+            #else 0.5 * (sdata[n // 2 - 1] + sdata[n // 2]))
+
+
 def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
     sdata = sorted(data)
     n = len(sdata)
     if n == 0:
         raise ValueError
-    return (sdata[n // 2] if n % 2 == 1
-            else 0.5 * (sdata[n // 2 - 1] + sdata[n // 2]))
+    return (sdata[n//2] if n % 2 == 1
+        else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
 
 
-def test_one_el():
+def test_one_element():
+    """
+    A test that check that the correct median is returned
+    for a one-element list
+    """
     test_list = [random.randint(0, 9)]
-    assert median(test_list) == test_list[0], 'Not correct for lists with ' \
-                                              'only one element.'
+    assert median(test_list) == test_list[0]
 
 
 def test_odd_numbers():
-    test_list = [i for i in range(0, 7)]
+    """
+    test that check that the correct median is returned for
+    a list with odd numbers of elements
+    """
+
+    test_list = [i for i in range(0, 5)]
     import statistics
-    assert median(test_list) == statistics.median(test_list), \
-        'Not correct for ' \
-        'odd numbers in' \
-        'list.'
+    assert median(test_list) == statistics.median(test_list)
 
 
 def test_even_numbers():
+    """
+    test that check that the correct median is returned for
+    a list with even numbers of elements
+    """
     import statistics
     test_list = [i for i in range(0, 6)]
-    assert median(test_list) == statistics.median(test_list), \
-        'Not correct for' \
-        'even numbers in' \
-        'list.'
+    assert median(test_list) == statistics.median(test_list)
 
 
 def test_ordered_elements():
+    """
+    test that check that list with ordered,
+    reverse-ordered and unordered elements returns the correct median
+    """
     import statistics
     test_list = [i for i in range(0, 8)]
-    assert median(test_list) == statistics.median(test_list), \
-        'Not correct for' \
-        'ordered elements' \
-        'in list'
+    assert median(test_list) == statistics.median(test_list)
 
 
 def test_rev_ordered_elements():
+    """
+    A test checking that requesting the median
+    of an empty list raises a ValueError exception
+    """
     test_list = [i for i in range(0, 8)]
     rev_test_list = sorted(test_list, reverse=True)
-    assert median(test_list) == median(rev_test_list), \
-        'Not correct for ' \
-        'reversed ordered' \
-        'elements in list.'
+    assert median(test_list) == median(rev_test_list)
 
 
 def test_unordered_elements():
+    """
+    A test that ensures that the median function
+    leaves the original data unchanged.
+    """
     import statistics
     test_list = [1, 5, 6, 8, 3, 65, 7, 4, 3, 2, 5, 6]
-    assert median(test_list) == statistics.median(test_list), \
-        'Not correct for ' \
-        'unordered lists.'
+    assert median(test_list) == statistics.median(test_list)
 
 
 def test_empty_list():
+    """
+    A test that ensures that the median function
+    works for tuples as well as lists
+    """
     with pytest.raises(ValueError):
         median([])
 
 
 def test_original_unchanged():
+
     test_list = [i for i in range(1, 9)]
     median(test_list)
     assert test_list == [i for i in range(1, 9)], 'The original data has ' \
--- src/fabian_nemazi_ex/ex03_project/test_sorting.py
+++ src/fabian_nemazi_ex/ex03_project/test_sorting.py
@@ -2,6 +2,7 @@ __author__ = 'Fabian Nemazi'
 __email__ = 'fabinema@nmbu.no'
 
 
+
 def bubble_sort(in_data):
     s_data = list(in_data)
     for j in reversed(range(len(s_data))):
@@ -12,63 +13,44 @@ def bubble_sort(in_data):
 
 
 def test_empty():
-    assert bubble_sort(()) == [], 'Test to see if bubble_sort works on' \
-                                  'empty lists'
-    assert bubble_sort([]) == [], 'Test to see if bubble_sort works on' \
-                                  'empty lists'
+    """Test that the sorting function works for empty list"""
+    assert bubble_sort(()) == []
+    assert bubble_sort([]) == []
 
 
 def test_single():
-    assert bubble_sort([1]) == [1], 'Testing bubble_sort for one-element' \
-                                    'lists.'
+    """Testing that the sorting function works for single-element list"""
+    assert bubble_sort([1]) == [1]
 
 
 def test_sorted_is_not_original():
-    """Test that the sorting function returns a new object."""
-    first_list = [1, 9, 17, 4, 7, 8]
+    """Testing that the sorting function returns a new object."""
+    first_list = [3, 2, 1]
     new_list = bubble_sort(first_list)
-    assert new_list != first_list, 'Testing bubble_sort to see if sorting' \
-                                   'function returns new object.'
+    assert new_list != first_list
 
 
 def test_original_unchanged():
-    """Test that sorting leaves the original data unchanged."""
-    first_list = [1, 9, 17, 4, 7, 8]
+    """Testing that sorting leaves the original data unchanged."""
+    first_list = [28, 3, 4, 10, 8]
     bubble_sort(first_list)
-    assert first_list == [1, 9, 17, 4, 7, 8], 'Test that sorting leaves' \
-                                              'original data unchanged'
-
+    assert first_list == [28, 3, 4, 10, 8]
 
 def test_sort_sorted():
-    """Test that sorting works on sorted data."""
-    assert bubble_sort([1, 2, 3, 4, 5, 10, 17]) == [1, 2, 3, 4, 5, 10, 17], \
-        'Test that sorting works on sorted data'
+    """Testing that sorting works on sorted data."""
+    assert bubble_sort([1, 2, 3, 4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+
 
 
 def test_sort_reversed():
-    """Test that sorting works on reverse-sorted data."""
-    assert bubble_sort([10, 9, 8, 7, 6]) == [6, 7, 8, 9, 10], \
-        'Test sorting for reversed data'
+    """Testing that sorting works on reverse-sorted data."""
+    assert bubble_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+
 
 
 def test_sort_all_equal():
-    """Test that sorting handles data with identical elements."""
-    assert bubble_sort([1, 8, 3, 4, 5, 6, 8, 2, 3]) == [1, 2, 3, 3, 4, 5, 6, 8,
-                                                        8], \
-        'Test sorting function to see if it' \
-        'works for identical element in list'
-
-
-def test_for_negative_elements():
-    test_list = [4, 3, 7, 1, -4, -2, 0]
-    assert bubble_sort(test_list) == [-4, -2, 0, 1, 3, 4, 7], 'Test for negative' \ 'numbers'
-
-
-def test_for_letters():
-    try:
-        bubble_sort(['n', 'o', 'r', 'w', 'a', 'y'])
-    except ValueError:
-        print('You cannot use strings')
+    """Testing that sorting handles data with identical elements."""
+    assert bubble_sort([1, 1, 1, 3, 4, 10, 2, 3]) == [1, 1, 1, 2, 3, 3, 4, 10]
 
 
 
